ed09e3daee47b624b935da613165ef3fb9a7fef7
BevFoss <100765704+BevFoss@users.noreply.github.com>
2022-03-08 13:30:24 +0200

    Initial Commit

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+//Hamburger menu toggle
+  $(".navbar-nav li a").click(function (event) {
+    // check if window is small enough so dropdown is created
+    var toggle = $(".navbar-toggle").is(":visible");
+    if (toggle) {
+      $(".navbar-collapse").collapse('hide');
+    }
+  });
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
